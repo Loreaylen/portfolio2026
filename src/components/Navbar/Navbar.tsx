7be42b98635816364768617e86b8da8beced0753
Loreaylen <lag9512@gmail.com>
2026-09-20 19:34:36 -0300

    useTranslation implement on Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,20 @@
 import './navbar.css'
+import { useTranslation } from 'react-i18next'
 
 const Navbar = () => {
+
+  const { t } = useTranslation('translation', { keyPrefix: 'nav' })
+  
+
   return (<nav id='menu'>
     <ul>
-      <li><a href="#home">Inicio</a></li>
-      <li><a href='#about'>Sobre mí</a></li>
-      <li><a href='#workExperience'>Experiencia</a></li>
-      <li><a href='#proyects'>Proyectos</a></li>
-      <li><a href='#skills'>Habilidades</a></li>
-      <li><a href='#education'>Formación</a></li>
-      <li><a href='#contact'>Contacto</a></li>
+      <li><a href="#home">{t('home')}</a></li>
+      <li><a href='#about'>{t('about')}</a></li>
+      <li><a href='#workExperience'>{t('experience')}</a></li>
+      <li><a href='#proyects'>{t('projects')}</a></li>
+      <li><a href='#skills'>{t('skills')}</a></li>
+      <li><a href='#education'>{t('education')}</a></li>
+      <li><a href='#contact'>{t('contact')}</a></li>
     </ul>
     </nav>)
 }
