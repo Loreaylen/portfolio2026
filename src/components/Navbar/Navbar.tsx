436e30299d2c52ced5de44812d17de6fb487d93a
Loreaylen <lag9512@gmail.com>
2026-09-20 11:04:41 -0300

    Move folders. Create navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import './navbar.css'
+
+const Navbar = () => {
+  return (<nav id='menu'>
+    <ul>
+      <li><a href="#home">Inicio</a></li>
+      <li><a href='#about'>Sobre mí</a></li>
+      <li><a href='#workExperience'>Experiencia</a></li>
+      <li><a href='#proyects'>Proyectos</a></li>
+      <li><a href='#skills'>Habilidades</a></li>
+      <li><a href='#education'>Formación</a></li>
+      <li><a href='#contact'>Contacto</a></li>
+    </ul>
+    </nav>)
+}
+
+export default Navbar
