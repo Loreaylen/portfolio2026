fcc73f26ce191e56e971ae0583194339c064b868
Loreaylen <lag9512@gmail.com>
2026-09-20 14:15:02 -0300

    Import i18n and config

---
+++ src/i18n/config.ts
@@ -0,0 +1,16 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector'
+import en from './en.json'
+import es from './es.json'
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: 'es',
+    resources: {
+      en:  {translation: en},
+      es:  {translation: es}
+    }
+  })
