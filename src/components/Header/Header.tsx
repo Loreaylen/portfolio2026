f33e5236ec3f2f15c4a2160e642dacff7b67d3b6
Loreaylen <lag9512@gmail.com>
2026-09-21 20:55:35 -0300

    feat: add accesible hamburguer menu toggle

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import Navbar from "../Navbar/Navbar"
 import { useTranslation } from "react-i18next"
+import { useState } from "react"
 
 import sunIcon from '@/assets/icons/sol.svg'
+import moonIcon from '@/assets/icons/moon.svg'
+import burguerMenu from '@/assets/icons/burgerMenu.svg'
 
 const Header = () => {
-  const { i18n } = useTranslation()
+  const { i18n, t } = useTranslation('translation', { keyPrefix: 'common' })
+  const [isOpen, useIsOpen] = useState(false)
 
   const toggleLanguage = () => {
     const currentLanguage: string | undefined = i18n.resolvedLanguage
@@ -14,11 +18,20 @@ const Header = () => {
     else i18n.changeLanguage('es')
   }
 
+  const toggleMenu = () => {
+    useIsOpen(!isOpen)
+    console.log(isOpen)
+  }
+
   return (
     <header id='header'>
-      <span><img src={sunIcon} alt="Menu" /></span>
+      <button aria-label={isOpen ? t('menuClose') : t('menuOpen')} onClick={() => { toggleMenu() }} ><img src={burguerMenu} alt="Menu" /></button>
       <button onClick={() => { toggleLanguage() }}>{i18n.resolvedLanguage?.toUpperCase()}</button>
       <Navbar />
+      <div>
+        <span><img src={sunIcon} alt={t('themeLight')} /></span>
+        <span><img src={moonIcon} alt={t('themeDark')} /></span>
+      </div>
     </header>
   )
 }
