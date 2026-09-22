6ddecae6771cad74a3081e1df0485c78ec69427c
Loreaylen <lag9512@gmail.com>
2026-09-21 22:18:54 -0300

    feat: add Hero. Add hero translations and common actions

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,27 @@
+import { useTranslation } from "react-i18next";
+
+const Hero = () => {
+
+  const { t } = useTranslation()
+
+
+  return (
+    <section id='hero'>
+      <div className='title-container'>
+        <h1>{t('hero.title')}</h1>
+        <h2>{t('hero.role')}</h2>
+        <span>{t('hero.focus')}</span>
+      </div>
+      <div className='actions-container'>
+        <button aria-label={t('common.linkedin')}></button>
+        <button aria-label={t('common.github')}></button>
+        <button aria-label={t('common.downloadCv')}></button>
+      </div>
+      <div className="avatar-container">
+        <img src="" alt="avatar" />
+      </div>
+    </section>
+  )
+}
+
+export default Hero
