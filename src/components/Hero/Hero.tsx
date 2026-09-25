af32d75923fa7f993ab77ab66fb2102fbbd8ab02
Loreaylen <lag9512@gmail.com>
2026-09-25 00:04:10 -0300

    feat: add bilingual CV download

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,17 @@
 import { useTranslation } from "react-i18next";
+import githubIcon from '@/assets/icons/github.png'
+import linkedinIcon from '@/assets/icons/linkedin.png'
+
 
 const Hero = () => {
 
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
+  const language:string = i18n.resolvedLanguage || 'en'
 
+  let path:string = ''
+  if(language === 'en'){
+    path= "/cv/AylenGil_CV_EN.pdf"
+  } else path= "/cv/AylenGil_CV_ES.pdf"
 
   return (
     <section id='hero'>
@@ -13,13 +21,13 @@ const Hero = () => {
         <span>{t('hero.focus')}</span>
       </div>
       <div className='actions-container'>
-        <button aria-label={t('common.linkedin')}></button>
-        <button aria-label={t('common.github')}></button>
-        <a href='#' aria-label={t('common.downloadCvAria')}>{t('common.downloadCv')}</a>
+        <button aria-label={t('common.linkedin')}><img src={githubIcon} alt="" /></button>
+        <button aria-label={t('common.github')}><img src={linkedinIcon} alt="" /></button>
+        <a href={path} download aria-label={t('common.downloadCvAria')}>{t('common.downloadCv')}</a>
       </div>
-      <div className="avatar-container">
+     {/*  <div className="avatar-container">
         <img src="" alt="avatar" />
-      </div>
+      </div> */}
     </section>
   )
 }
